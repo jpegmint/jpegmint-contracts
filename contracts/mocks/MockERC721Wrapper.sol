e756b322c6b12c4dc96a61d69fa887fab804474c
JPΞG <62081548+jpegdigital@users.noreply.github.com>
2021-08-06 00:19:56 -0500

    Imported wrapper contracts.

---
+++ contracts/mocks/MockERC721Wrapper.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+/// @author jpegmint.xyz
+
+import "../wrapper/ERC721Wrapper.sol";
+
+contract MockERC721Wrapper is ERC721Wrapper {
+    
+    constructor() ERC721("MockERC721Wrapper", "wMOCK") {}
+
+    function updateApprovedTokenRanges(address contract_, uint256 minTokenId, uint256 maxTokenId) public override {
+        _updateApprovedTokenRanges(contract_, minTokenId, maxTokenId);
+    }
+}
