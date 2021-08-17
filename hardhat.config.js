9a6aa19dd5e12681304084f333ac26265743bcbb
JPΞG <62081548+jpegdigital@users.noreply.github.com>
2021-08-17 13:56:24 -0500

    Standardized compiler versions.

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ const { etherscanApiKey, alchemyApiKey, mnemonic } = require('./secrets.json');
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,
