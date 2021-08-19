dff22daec98b27998d74803d63f7a4e5b05f8fa6
JPΞG <62081548+jpegdigital@users.noreply.github.com>
2021-08-19 00:44:55 -0500

    Refactored and tested GremlinsAirdrop

--- contracts/gremlins/GremlinsAirdrop.sol
+++ contracts/gremlins/GremlinsAirdrop.sol
@@ -6,29 +6,20 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../token/ERC721/ERC721EnumerableOrdered.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
+contract GremlinsAirdrop is ERC721, ERC721Enumerable, AccessControl {
 
     /// Variables ///
     string internal _metadataBaseURI;
     uint256 internal _tokenMaxSupply;
 	uint256[] private _tokenIdTracker;
 
-    /// Mappings ///
-    mapping(uint256 => string) private _tokenURIs;
-
     /// Roles ///
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    /**
-     * @dev Constructor.
-     */
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 tokenMaxSupply
-    )
+    /// Constructor ///
+    constructor(string memory name, string memory symbol, uint256 tokenMaxSupply)
     ERC721(name, symbol) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(ADMIN_ROLE, msg.sender);
@@ -41,14 +32,13 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
     //================================================================================
 
     /**
-     * @dev Core redeeming and minting function.
-     * todo Add wallet access control and redemption max.
+     * @dev Airdrops tokens to specified wallet addresses. Batching supported.
      */
-    function airdrop(address[] memory airdropWallets) external onlyRole(ADMIN_ROLE) {
-        require(airdropWallets.length + totalSupply() <= _tokenMaxSupply, "Airdrop: Too many mints");
-        for (uint256 i = 0; i < airdropWallets.length; i++) {
+    function airdrop(address[] memory wallets) external onlyRole(ADMIN_ROLE) {
+        require(availableSupply() >= wallets.length, "Airdrop: More wallets provided than available supply");
+        for (uint256 i = 0; i < wallets.length; i++) {
             uint256 tokenId = _generateTokenId();
-            _safeMint(airdropWallets[i], tokenId);
+            _mint(wallets[i], tokenId);
         }
     }
 
@@ -56,9 +46,8 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
      * @dev Generate random tokenIds using Meebits random ID strategy.
      */
     function _generateTokenId() private returns (uint256) {
-        uint256 remainingQty = _tokenMaxSupply - totalSupply();
-        uint256 randomNum = uint256(keccak256(abi.encodePacked(msg.sender, tx.gasprice, block.timestamp, remainingQty)));
-        uint256 randomIndex = randomNum % remainingQty;
+        uint256 remainingQty = availableSupply();
+        uint256 randomIndex = _generateRandomNum(remainingQty) % remainingQty;
 
         // If array value exists, use, otherwise use generated random value.
         uint256 existingValue = _tokenIdTracker[randomIndex];
@@ -72,6 +61,16 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
         return tokenId + 1; // Start tokens at #1
     }
 
+    /**
+     * @dev Generate pseudorandom number via various transaction properties.
+     */
+    function _generateRandomNum(uint256 seed) internal view virtual returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(msg.sender, tx.gasprice, block.timestamp, seed)));
+    }
+
+    /**
+     * @dev Helper function to pair with total supply.
+     */
     function availableSupply() public view returns (uint256) {
         return _tokenMaxSupply - totalSupply();
     }
@@ -80,43 +79,18 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
     // Metadata Functions
     //================================================================================
 
+    /**
+     * @dev Store and update new base uri.
+     */
     function setBaseURI(string memory newURI) external onlyRole(ADMIN_ROLE) {
         _metadataBaseURI = newURI;
     }
 
+    /**
+     * @dev Return the base URI for OpenZeppelin default TokenURI implementation.
+     */
     function _baseURI() internal view override returns (string memory) {
         return _metadataBaseURI;
-    }
-
-    /**
-     * @dev Generates tokenURI using baseURI and tokenURI combinations.
-     */
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "Airdrop: URI query for nonexistent token");
-
-        string memory _tokenURI = _tokenURIs[tokenId];
-        string memory base = _baseURI();
-
-        // If there is no base URI, return the token URI.
-        if (bytes(base).length == 0) {
-            return _tokenURI;
-        }
-        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
-        if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(base, _tokenURI));
-        }
-
-        return super.tokenURI(tokenId);
-    }
-
-    /**
-     * @dev Sets tokenURIs for future metadata updates.
-     */
-    function setTokenURIs(string[] memory tokenURIs) external onlyRole(ADMIN_ROLE) {
-        require(tokenURIs.length == _tokenMaxSupply, "Airdrop: Too many URIs provided");
-        for (uint256 i = 0; i < tokenURIs.length; i++) {
-            _tokenURIs[i] = tokenURIs[i];
-        }
     }
 
     //================================================================================
@@ -125,7 +99,7 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        override(ERC721, ERC721EnumerableOrdered)
+        override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
@@ -133,7 +107,7 @@ contract GremlinsAirdrop is ERC721, ERC721EnumerableOrdered, AccessControl  {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721EnumerableOrdered, AccessControl)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
--- test/gremlins/GremlinsAirdrop.test.js
+++ test/gremlins/GremlinsAirdrop.test.js
@@ -1,6 +1,5 @@
 // Load dependencies
 const { expect } = require("chai");
-const { keccak256 } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
 /**
@@ -14,6 +13,11 @@ describe('GremlinsAirdrop', function () {
 
     before(async function () {
         [this.owner, this.addr1, this.gnosis, this.owner1, this.owner2] = await ethers.getSigners();
+
+        this.wallets = [];
+        for (let i = 0; i < TOKEN_MAX_SUPPLY; i++) {
+            this.wallets.push(this.owner.address);
+        }
 
         this.factory = await ethers.getContractFactory("MockGremlinsAirdrop");
     });
@@ -33,7 +37,8 @@ describe('GremlinsAirdrop', function () {
             expect(await this.contract.symbol()).to.equal(CONTRACT_SYMBOL);
         });
 
-        it('correctly sets admin role', async function () {
+        it('correctly sets admin roles', async function () {
+            expect(await this.contract.hasRole(ethers.constants.HashZero, this.owner.address)).to.be.true;
             expect(await this.contract.hasRole(ROLE_ADMIN, this.owner.address)).to.be.true;
         });
 
@@ -43,85 +48,115 @@ describe('GremlinsAirdrop', function () {
         });
     });
 
-    // describe('minting', function () {
+    describe('minting', function () {
 
-    //     it('correctly reserves tokens', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY - TOKEN_MAX_RESERVED);
-    //     });
+        it('correctly mints all', async function() {
+            await this.contract.airdrop(this.wallets);
+            expect(await this.contract.totalSupply()).to.equal(TOKEN_MAX_SUPPLY);
+            expect(await this.contract.availableSupply()).to.equal(0);
+            expect(await this.contract.balanceOf(this.owner.address)).to.equal(TOKEN_MAX_SUPPLY);
+        });
 
-    //     it('correctly mints after reserved', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         await this.contract.startSale();
-    //         await this.contract.mintCollectibles(1);
-            
-    //         expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY - TOKEN_MAX_RESERVED - 1);
-    //     });
-    // });
+        it('correctly mints in batches', async function() {
+            await this.contract.airdrop(this.wallets.slice(0, 25));
+            expect(await this.contract.totalSupply()).to.equal(25);
 
-    // describe('enumerable', function () {
+            await this.contract.airdrop(this.wallets.slice(0, 25));
+            expect(await this.contract.totalSupply()).to.equal(50);
 
-    //     it('correctly tracks totalSupply', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         await this.contract.startSale();
-    //         expect(await this.contract.totalSupply()).to.equal(10);
-    //         expect(await this.contract.availableSupply()).to.equal(246);
+            await this.contract.airdrop(this.wallets.slice(0, 25));
+            expect(await this.contract.totalSupply()).to.equal(75);
 
-    //         for (var i = 0; i < 246; i++) {
-    //             await this.contract.mintCollectibles(1);
-    //             expect(await this.contract.totalSupply()).to.equal(i + 11);
-    //             expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY - 11 - i);
-    //         }
+            expect(await this.contract.availableSupply()).to.equal(0);
+            expect(await this.contract.balanceOf(this.owner.address)).to.equal(75);
+        });
 
-    //         expect(this.contract.mintCollectibles(1)).to.be.reverted;
-    //         expect(await this.contract.totalSupply()).to.equal(TOKEN_MAX_SUPPLY);
-    //         expect(await this.contract.availableSupply()).to.equal(0);
-    //     });
+        it('correctly mints one at a time', async function() {
+            await this.contract.airdrop(this.wallets.slice(0, 1));
+            expect(await this.contract.totalSupply()).to.equal(1);
+            expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY - 1);
+        });
 
-    //     it('correctly tracks token indexes', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         await this.contract.startSale();
-    //         expect(await this.contract.tokenByIndex(9)).to.equal(10);
+        it('correctly mints even if non-ERC721 receiver', async function() {
+            factory = await ethers.getContractFactory("ERC721");
+            erc721 = await factory.deploy('Mock', 'MOCK');
+            await erc721.deployed();
 
-    //         for (var i = 0; i < 246; i++) {
-    //             await this.contract.mintCollectibles(1);
-    //             expect(await this.contract.tokenByIndex(i + 10)).to.equal(i + 11);
-    //         }
+            let wallets = this.wallets.slice(0,74);
+            wallets.push(erc721.address);
+            await this.contract.airdrop(wallets);
+            expect(await this.contract.totalSupply()).to.equal(TOKEN_MAX_SUPPLY);
+            expect(await this.contract.availableSupply()).to.equal(0);
+        });
 
-    //         expect(this.contract.mintCollectibles(1)).to.be.reverted;
-    //         expect(await this.contract.tokenByIndex(TOKEN_MAX_SUPPLY - 1)).to.equal(TOKEN_MAX_SUPPLY);
-    //     });
+        it('correctly mints to multiple wallets', async function() {
+            let wallets = [this.owner.address, this.addr1.address, this.owner1.address, this.owner2.address];
+            await this.contract.airdrop(wallets);
+            expect(await this.contract.totalSupply()).to.equal(4);
+            expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY - 4);
+            for (let i = 0; i < wallets.length; i++) {
+                expect(await this.contract.balanceOf(wallets[i])).to.equal(1);
+            }
+        });
 
-    //     it('correctly tracks owner token indexes', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         await this.contract.startSale();
-    //         expect(await this.contract.tokenOfOwnerByIndex(this.owner.address, 9)).to.equal(10);
+        it('correctly mints from another admin', async function() {
+            await this.contract.grantRole(ROLE_ADMIN, this.addr1.address);
+            await this.contract.connect(this.addr1).airdrop(this.wallets);
+            expect(await this.contract.connect(this.addr1).totalSupply()).to.equal(TOKEN_MAX_SUPPLY);
+            expect(await this.contract.connect(this.addr1).availableSupply()).to.equal(0);
+            expect(await this.contract.connect(this.addr1).balanceOf(this.owner.address)).to.equal(TOKEN_MAX_SUPPLY);
+        });
 
-    //         for (var i = 0; i < 246; i++) {
-    //             await this.contract.mintCollectibles(1);
-    //             expect(await this.contract.tokenOfOwnerByIndex(this.owner.address, i + 10)).to.equal(i + 11);
-    //         }
+        it('reverts if non-admin mints', async function() {
+            await expect(this.contract.connect(this.addr1).airdrop(this.wallets.slice(0, 1))).to.be.reverted;
+            expect(await this.contract.totalSupply()).to.equal(0);
+            expect(await this.contract.availableSupply()).to.equal(TOKEN_MAX_SUPPLY);
+        });
 
-    //         expect(this.contract.mintCollectibles(1)).to.be.reverted;
-    //         expect(await this.contract.tokenOfOwnerByIndex(this.owner.address, TOKEN_MAX_SUPPLY - 1)).to.equal(TOKEN_MAX_SUPPLY);
-    //     });
+        it('reverts if over minted', async function() {
+            await this.contract.airdrop(this.wallets);
+            await expect(this.contract.airdrop(this.wallets)).to.be.revertedWith('Airdrop: More wallets provided than available supply');
+            await expect(this.contract.airdrop(this.wallets.slice(0, 1))).to.be.revertedWith('Airdrop: More wallets provided than available supply');
+            expect(await this.contract.totalSupply()).to.equal(TOKEN_MAX_SUPPLY);
+            expect(await this.contract.availableSupply()).to.equal(0);
+            expect(await this.contract.balanceOf(this.owner.address)).to.equal(TOKEN_MAX_SUPPLY);
+        });
+    });
 
-    //     it('correctly tracks mixed owner token indexes', async function () {
-    //         await this.contract.reserveCollectibles();
-    //         await this.contract.startSale();
-    //         expect(await this.contract.tokenOfOwnerByIndex(this.owner.address, 9)).to.equal(10);
+    describe('metadata', function () {
+        it('correctly updates baseURI', async function() {
+            await this.contract.airdrop(this.wallets.slice(0, 1));
+            const tokenId = await this.contract.tokenByIndex(0);
+            await this.contract.setBaseURI('base/');
+            expect(await this.contract.tokenURI(tokenId)).to.equal('base/' + tokenId.toString());
+        });
 
-    //         for (var i = 0; i < 10; i++) {
-    //             await this.contract.connect(this.owner1).mintCollectibles(1);
-    //             expect(await this.contract.tokenOfOwnerByIndex(this.owner1.address, i)).to.equal(i + 1 + 10);
-    //         }
-            
-    //         for (var i = 0; i < 10; i++) {
-    //             await this.contract.connect(this.owner2).mintCollectibles(1);
-    //             expect(await this.contract.tokenOfOwnerByIndex(this.owner2.address, i)).to.equal(i + 1 + 10 + 10);
-    //         }
+        it('correctly updates baseURI multiple times', async function() {
+            await this.contract.grantRole(ROLE_ADMIN, this.addr1.address);
+            await this.contract.airdrop(this.wallets.slice(0, 1));
+            const tokenId = await this.contract.tokenByIndex(0);
+            await this.contract.setBaseURI('base/');
+            expect(await this.contract.tokenURI(tokenId)).to.equal('base/' + tokenId.toString());
+            await this.contract.connect(this.addr1).setBaseURI('base2/');
+            expect(await this.contract.tokenURI(tokenId)).to.equal('base2/' + tokenId.toString());
+            await this.contract.setBaseURI('base/');
+            expect(await this.contract.tokenURI(tokenId)).to.equal('base/' + tokenId.toString());
+        });
 
-    //         expect(await this.contract.tokenByIndex(29)).to.equal(30);
-    //     });
-    // });
+        it('correctly updates baseURI from another admin', async function() {
+            await this.contract.grantRole(ROLE_ADMIN, this.addr1.address);
+            await this.contract.connect(this.addr1).airdrop(this.wallets.slice(0, 1));
+            const tokenId = await this.contract.connect(this.addr1).tokenByIndex(0);
+            await this.contract.connect(this.addr1).setBaseURI('base/');
+            expect(await this.contract.connect(this.addr1).tokenURI(tokenId)).to.equal('base/' + tokenId.toString());
+        });
+
+        it('reverts if non-admin updates baseURI', async function() {
+            await this.contract.airdrop(this.wallets.slice(0, 1));
+            const tokenId = await this.contract.tokenByIndex(0);
+            await this.contract.setBaseURI('base/');
+            await expect(this.contract.connect(this.addr1).setBaseURI('notthis/')).to.be.reverted;
+            expect(await this.contract.tokenURI(tokenId)).to.equal('base/' + tokenId.toString());
+        });
+    });
 });
